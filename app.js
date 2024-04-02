dbd5bb8979ad5538d780d027601fb5fc0d37083d
Agamjyot Singh <80823915+agamjyotsingh120@users.noreply.github.com>
2024-04-02 18:30:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+function logout(){
+    window.location = "Password website.html"
+}
+
+function np(){
+    window.location = "np1.html"
+}
+function djf(){
+    window.location = "ngh.html"
+}
+function gff(){
+    window.location = "ghj.html"
+}
+
+function tlp3(){
+    window.location = "lp.html"
+}
+function op1(){
+    window.location = "lp1.html"
+}
